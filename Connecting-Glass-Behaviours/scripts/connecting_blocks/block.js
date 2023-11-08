eaedfa4a3139a4b1d5bdfb4d9867ceb6003b7ec7
MADLAD3718 <scheuringjack@gmail.com>
2023-11-07 19:38:25 -0500

    Fix LocationOutOfWorldBoundaries errors

--- Connecting-Glass-Behaviours/scripts/connecting_blocks/block.js
+++ Connecting-Glass-Behaviours/scripts/connecting_blocks/block.js
@@ -7,24 +7,12 @@ import { Block } from "@minecraft/server";
  * @param {Boolean} placed 
  */
 export function alterBlock(block, placed) {
-    const block_u = block.above();
-    const block_d = block.below();
+    const { location, dimension } = block;
+
     const block_n = block.north();
     const block_s = block.south();
     const block_e = block.east();
     const block_w = block.west();
-
-    if (block_u.hasTag("connecting_glass")) {
-        const perm = block_u.permutation;
-        const ud = perm.getState("cnct:cull_ud");
-        block_u.setPermutation(perm.withState("cnct:cull_ud", placed ? setBit(ud, 0) : resetBit(ud, 0)));
-    }
-
-    if (block_d.hasTag("connecting_glass")) {
-        const perm = block_d.permutation;
-        const ud = perm.getState("cnct:cull_ud");
-        block_d.setPermutation(perm.withState("cnct:cull_ud", placed ? setBit(ud, 1) : resetBit(ud, 1)));
-    }
 
     if (block_n.hasTag("connecting_glass")) {
         const perm = block_n.permutation;
@@ -48,5 +36,23 @@ export function alterBlock(block, placed) {
         const perm = block_w.permutation;
         const nsew = perm.getState("cnct:cull_nsew");
         block_w.setPermutation(perm.withState("cnct:cull_nsew", placed ? setBit(nsew, 1) : resetBit(nsew, 1)));
+    }
+
+    if (location.y < dimension.heightRange.max) {
+        const block_u = block.above();
+        if (block_u.hasTag("connecting_glass")) {
+            const perm = block_u.permutation;
+            const ud = perm.getState("cnct:cull_ud");
+            block_u.setPermutation(perm.withState("cnct:cull_ud", placed ? setBit(ud, 0) : resetBit(ud, 0)));
+        }
+    }
+
+    if (location.y > dimension.heightRange.min) {
+        const block_d = block.below();
+        if (block_d.hasTag("connecting_glass")) {
+            const perm = block_d.permutation;
+            const ud = perm.getState("cnct:cull_ud");
+            block_d.setPermutation(perm.withState("cnct:cull_ud", placed ? setBit(ud, 1) : resetBit(ud, 1)));
+        }
     }
 }
--- Connecting-Glass-Behaviours/scripts/connecting_blocks/connecting_glass.js
+++ Connecting-Glass-Behaviours/scripts/connecting_blocks/connecting_glass.js
@@ -8,35 +8,17 @@ import { Block } from "@minecraft/server";
  */
 export function alterConnectingGlass(block, placed) {
     let cull_ud = 0, cull_nsew = 0, cnct_ud = 0, cnct_nsew = 0;
-    const { permutation } = block;
+    const { permutation, location, dimension } = block;
 
-    const block_u = block.above();
-    const block_d = block.below();
     const block_n = block.north();
     const block_s = block.south();
     const block_e = block.east();
     const block_w = block.west();
 
-    cull_ud |= block_u.isSolid << 1;
-    cull_ud |= block_d.isSolid << 0;
     cull_nsew |= block_n.isSolid << 3;
     cull_nsew |= block_s.isSolid << 2;
     cull_nsew |= block_e.isSolid << 1;
     cull_nsew |= block_w.isSolid << 0;
-
-    if (block_u.hasTag("connecting_glass")) {
-        cnct_ud = setBit(cnct_ud, 1);
-        const perm = block_u.permutation;
-        const ud = perm.getState("cnct:cnct_ud");
-        block_u.setPermutation(perm.withState("cnct:cnct_ud", placed ? setBit(ud, 0) : resetBit(ud, 0)));
-    }
-
-    if (block_d.hasTag("connecting_glass")) {
-        cnct_ud = setBit(cnct_ud, 0);
-        const perm = block_d.permutation;
-        const ud = perm.getState("cnct:cnct_ud");
-        block_d.setPermutation(perm.withState("cnct:cnct_ud", placed ? setBit(ud, 1) : resetBit(ud, 1)));
-    }
 
     if (block_n.hasTag("connecting_glass")) {
         cnct_nsew = setBit(cnct_nsew, 3);
@@ -64,6 +46,28 @@ export function alterConnectingGlass(block, placed) {
         const perm = block_w.permutation;
         const nsew = perm.getState("cnct:cnct_nsew");
         block_w.setPermutation(perm.withState("cnct:cnct_nsew", placed ? setBit(nsew, 1) : resetBit(nsew, 1)));
+    }
+
+    if (location.y < dimension.heightRange.max) {
+        const block_u = block.above();
+        cull_ud |= block_u.isSolid << 1;
+        if (block_u.hasTag("connecting_glass")) {
+            cnct_ud = setBit(cnct_ud, 1);
+            const perm = block_u.permutation;
+            const ud = perm.getState("cnct:cnct_ud");
+            block_u.setPermutation(perm.withState("cnct:cnct_ud", placed ? setBit(ud, 0) : resetBit(ud, 0)));
+        }
+    }
+
+    if (location.y > dimension.heightRange.min) {
+        const block_d = block.below();
+        cull_ud |= block_d.isSolid << 0;
+        if (block_d.hasTag("connecting_glass")) {
+            cnct_ud = setBit(cnct_ud, 0);
+            const perm = block_d.permutation;
+            const ud = perm.getState("cnct:cnct_ud");
+            block_d.setPermutation(perm.withState("cnct:cnct_ud", placed ? setBit(ud, 1) : resetBit(ud, 1)));
+        }
     }
 
     if (placed) block.setPermutation(permutation.withState("cnct:cull_ud", cull_ud)
